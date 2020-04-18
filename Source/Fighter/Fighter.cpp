2f2aba1d36a246a97ac0d8bba23bf4b2cd49ef0f
Kunal SAgar <kunalsagar1999@gmail.com>
2020-04-18 17:30:45 +0530

    init

---
+++ Source/Fighter/Fighter.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Fighter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fighter, "Fighter" );
