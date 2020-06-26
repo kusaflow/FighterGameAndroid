6c3a797255dc52480f69a65d0bd9ef6858f170bf
Kunal SAgar <kunalsagar1999@gmail.com>
2020-06-26 23:43:09 +0530

    backup

--- Source/Fighter/Fighter.Build.cs
+++ Source/Fighter/Fighter.Build.cs
@@ -12,9 +12,15 @@ public class Fighter : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+			PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "OnlineSubsystem" });
+			DynamicallyLoadedModuleNames.Add("OnlineSubsystemGooglePlay");
+		}
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
